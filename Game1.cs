79b8491d5585c2d9fe5f5e87560b8f266fb4e3c5
okdae <okdae@AMEERAH>
2025-05-30 18:53:07 -0700

    Add project files.

---
+++ Game1.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using MonoGame.Extended;
+
+namespace Platformer
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            // TODO: Add your initialization logic here
+
+            base.Initialize();
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            // TODO: use this.Content to load your game content here
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed || Keyboard.GetState().IsKeyDown(Keys.Escape))
+                Exit();
+
+            // TODO: Add your update logic here
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.DarkGreen);
+            _spriteBatch.Begin();
+            _spriteBatch.DrawCircle(10,10,100,10,Color.Black);
+            _spriteBatch.End();
+            // TODO: Add your drawing code here
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Sprite.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Platformer
+{
+    public abstract class Sprite
+    {
+        public Vector2 Location { get; set; }
+        public Vector2 Size { get; set; }
+        public Texture2D Texture { get; set; }
+        public Vector2 Speed;
+
+
+        protected Sprite(Vector2 location, Vector2 size, Texture2D texture, Vector2 speed)
+        {
+            Location = location;
+            Size = size;
+            Texture = texture;
+            Speed = speed;
+        }
+
+        public abstract void Update(GameTime gameTime, KeyboardState KeyState, Viewport Viewport);
+
+        public abstract void Draw(GameTime gameTime, SpriteBatch spriteBatch);
+    }
+}
